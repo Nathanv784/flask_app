c6ef356fd7514196a20cf45286340e3fb5eb2e64
nathanv784 <nathanvirushabadoss@bitcot.com>
2024-02-09 10:18:43 +0530

    updated

--- app.py
+++ app.py
@@ -1,27 +1,12 @@
+# app.py
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
+from db import db
+from models.class import User, Note
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:nathan7845@localhost/postgres'
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    # Establishing one-to-many relationship with notes
-    notes = db.relationship('Note', backref='user', lazy=True)
-
-class Note(db.Model):
-    id = db.Column(db.Integer, db.ForeignKey('user.id'),primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
-    content = db.Column(db.Text, nullable=False)
-    # user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-
-with app.app_context():
-    db.create_all()
+db.init_app(app)
 
 @app.route('/')
 def hello_world():
